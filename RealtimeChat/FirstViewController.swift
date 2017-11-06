5c9a161e15b4eea626b21f4fff16f45387162e1a
Carlos Duclos <darkzeratul64@gmail.com>
2017-11-05 23:21:10 -0500

    first

---
+++ RealtimeChat/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RealtimeChat
+//
+//  Created by Carlos Duclos on 11/5/17.
+//  Copyright © 2017 Carlos Duclos. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
